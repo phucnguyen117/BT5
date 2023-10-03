e1ee28a24d273ecbb756f94d09cb42bd3033fe1d
phucnguyen117 <144077716+phucnguyen117@users.noreply.github.com>
2023-10-03 09:23:35 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+/**
+ * 
+ * @author HoangHuuPhucNguyen
+ *
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến 
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+
+		System.out.println("Ho va Ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		
+		//khai báo hằng. Hằng phải viết hoa 
+		final float PI = 3.14f; 
+	}
+
+}
